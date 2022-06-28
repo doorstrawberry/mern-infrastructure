b8cd66c6912423a203a17d484ce3b279dae8de09
doorstrawberry <kangminsoo671@gmail.com>
2022-06-28 08:55:25 -0400

    all

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,11 +1,24 @@
-function create(req, res) {
-    // Baby step...
-    res.json({
-        user: {
-            name: req.body.name,
-            email: req.body.email
-        }
-    });
+const User = require("../../models/user")
+const jwt = require('jsonwebtoken')
+
+async function create(req, res) {
+    try {
+        const user = await User.create(req.body)
+        const token = createJWT(user);
+        res.json(token);
+    }
+    catch (err) {
+        res.status(400).json(err)
+    }
+}
+
+function createJWT(user) {
+    return jwt.sign(
+        // data payload
+        { user },
+        process.env.SECRET,
+        { expiresIn: '24h' }
+    );
 }
 
 module.exports = {
